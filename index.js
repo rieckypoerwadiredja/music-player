6d0ae54ec84466f35f27eb6bcd3779404c71ccfa
rieckypoerwadiredja <rieckysyarief@gmail.com>
2022-02-16 21:10:30 +0700

    changed name

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get('/marketplace', (req, res) => {
     });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
